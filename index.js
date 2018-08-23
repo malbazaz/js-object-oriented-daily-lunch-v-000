9599ae91ce56d039cfda5a6c7e2a94174eabc3cb
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-23 22:15:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,8 +79,9 @@ class Meal{
           const uniqueCust = [...new Set(notUniqueCust)];
           return uniqueCust;
         }
+
         static byPrice(){
-          return store.meals.slice().sort(function(num1,num2){
+          return store.meals().slice().sort(function(num1,num2){
             debugger;
             return num1.price - num2.price;
           })
